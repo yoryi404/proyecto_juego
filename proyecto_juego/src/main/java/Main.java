a33dc9c10ec691cb5f092a38b5a73d55f7a50972
Hugo23-4 <hugoestudiospm1@gmail.com>
2025-05-03 16:12:02 +0200

    v2 en creacion fxml y modificacion en main

--- proyecto_juego/src/main/java/Main.java
+++ proyecto_juego/src/main/java/Main.java
@@ -1,10 +1,18 @@
+package com.jorge_hugo_javier;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/dungeon/view/MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/jorge_hugo_javier/Vistas/CreacionPersonaje.fxml"));
         Parent root = loader.load();
-        
-        primaryStage.setTitle("Juego de mazmorras");
+
+        primaryStage.setTitle("Juego de Mazmorras - Crear Personaje");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
