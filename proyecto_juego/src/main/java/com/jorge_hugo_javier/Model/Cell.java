dab963b86e24c226d773cac87f2ea313d6ca0518
yoryi404 <jalegrem04@iesalbarregas.es>
2025-05-05 13:09:40 +0200

    Enemigos y Celda

--- proyecto_juego/src/main/java/com/jorge_hugo_javier/Model/Cell.java
+++ proyecto_juego/src/main/java/com/jorge_hugo_javier/Model/Cell.java
@@ -1,5 +1,28 @@
 package com.jorge_hugo_javier.Model;
 
 public class Cell {
-    
+    public enum Type { WALL, FLOOR }
+
+    private Type type;
+    private Character occupant;
+
+    public Cell(Type type) {
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public boolean isWalkable() {
+        return type == Type.FLOOR && (occupant == null || occupant.isDead());
+    }
+
+    public void setOccupant(Character occupant) {
+        this.occupant = occupant;
+    }
+
+    public Character getOccupant() {
+        return occupant;
+    }
 }
