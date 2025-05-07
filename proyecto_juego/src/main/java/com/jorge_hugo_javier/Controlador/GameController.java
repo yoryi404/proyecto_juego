a71ae22361759d3fe0be0abc7627b4c2166af94b
Javiergg06 <javiergilgalan@gmail.com>
2025-05-07 12:53:46 +0200

    Controlador de juego

---
+++ proyecto_juego/src/main/java/com/jorge_hugo_javier/Controlador/GameController.java
@@ -0,0 +1,38 @@
+package com.jorge_hugo_javier.Controlador;
+import com.jorge_hugo_javier.Model.Jugador;
+import com.jorge_hugo_javier.Model.Enemigo;
+import com.jorge_hugo_javier.Model.JuegoMap;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+public class GameController {
+ @FXML
+ private GridPane gridPane;
+ private Jugador jugador;
+ private JuegoMap mapa;
+ public void setJugador(Jugador jugador) {
+ this.jugador = jugador;
+ }
+ public void setMapa(JuegoMap mapa) {
+ this.mapa = mapa;
+ actualizarVista();
+ }
+ public void manejarTeclado(KeyEvent evento) {
+ switch (evento.getCode()) {
+ case W -> jugador.moverArriba();
+ case S -> jugador.moverAbajo();
+ case A -> jugador.moverIzquierda();
+ case D -> jugador.moverDerecha();
+ }
+ actualizarVista();
+ moverEnemigos();
+ }
+ private void actualizarVista() {
+ System.out.println("Jugador en: " + jugador.getPosX() + "," + jugador.getPosY());
+ }
+ private void moverEnemigos() {
+ for (Enemigo e : mapa.getEnemigos()) {
+ e.moverHacia(jugador.getPosX(), jugador.getPosY());
+ }
+ }
+}
