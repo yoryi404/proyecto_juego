c1619711d800592e0883487d566286e1de9b27d4
yoryi404 <jalegrem04@iesalbarregas.es>
2025-05-10 04:00:12 +0200

    Revert "Merge branch 'Jorge'"
    This reverts commit 34849c53dcfb449d94e770868b2f96cb21388143, reversing
    changes made to 1a727067be1424d649bc1cb98e187e1eeeae65c7.

--- proyecto_juego/src/main/java/com/jorge_hugo_javier/Controlador/ControladorPrincipal.java
+++ proyecto_juego/src/main/java/com/jorge_hugo_javier/Controlador/ControladorPrincipal.java
@@ -43,13 +43,9 @@ public class ControladorPrincipal {
     private ListView<String> listaTurnos;
 
     private Jugador jugador;
-<<<<<<< HEAD
     protected JuegoMap mapa;
     private int jugadorFila;
     private int jugadorCol;
-=======
-    private JuegoMap mapa;
->>>>>>> Jorge
 
     // Método para recibir el jugador desde CreacionPersonaje
     public void setJugador(Jugador jugador) {
@@ -92,10 +88,8 @@ public class ControladorPrincipal {
      * Cargar el mapa desde el archivo Nivel1.txt
      */
     private void cargarMapaDesdeFichero() {
-        try {
-            mapa = new JuegoMap("src/main/resources/com/jorge_hugo_javier/Mapa/Nivel1.txt");
+        List<char[]> filas = new ArrayList<>();
 
-<<<<<<< HEAD
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(getClass().getResourceAsStream(
                         "/com/jorge_hugo_javier/Mapa/Nivel1.txt")))) {
@@ -108,12 +102,6 @@ public class ControladorPrincipal {
             mapa.addEnemigo(enemigo1);
             Enemigo enemigo2 = new Enemigo("Orco", 15, 3, 2, 1);
             mapa.addEnemigo(enemigo2);
-=======
-            // Añadir enemigos
-            Enemigo enemigo1 = new Enemigo("Goblin", 10, 2, 1, 1);
-            mapa.addEnemigo(enemigo1);
-
->>>>>>> Jorge
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -123,11 +111,10 @@ public class ControladorPrincipal {
      * Pintar el mapa en el GridPane
      */
     private void dibujarMapa() {
-    gridMapa.getChildren().clear();
-    gridMapa.getColumnConstraints().clear();
-    gridMapa.getRowConstraints().clear();
+        gridMapa.getChildren().clear();
+        gridMapa.getColumnConstraints().clear();
+        gridMapa.getRowConstraints().clear();
 
-<<<<<<< HEAD
         Cell[][] grid = mapa.getGrid();
         int numFilas = grid.length;
         int numCols = grid[0].length;
@@ -145,30 +132,10 @@ public class ControladorPrincipal {
         for (int fila = 0; fila < numFilas; fila++) {
             for (int col = 0; col < numCols; col++) {
                 Cell celda = grid[fila][col];
-=======
-    Cell[][] grid = mapa.getGrid();
-    int numFilas = grid.length;
-    int numCols = grid[0].length;
 
-    for (int i = 0; i < numCols; i++) {
-        ColumnConstraints colConst = new ColumnConstraints(40);
-        gridMapa.getColumnConstraints().add(colConst);
-    }
+                StackPane panel = new StackPane();
+                panel.setPrefSize(40, 40);
 
-    for (int i = 0; i < numFilas; i++) {
-        RowConstraints rowConst = new RowConstraints(40);
-        gridMapa.getRowConstraints().add(rowConst);
-    }
-
-    for (int fila = 0; fila < numFilas; fila++) {
-        for (int col = 0; col < numCols; col++) {
-            Cell celda = grid[fila][col];
->>>>>>> Jorge
-
-            StackPane panel = new StackPane();
-            panel.setPrefSize(40, 40);
-
-<<<<<<< HEAD
                 if (celda.getType() == Cell.Type.WALL) {
                     Image imgPared = new Image(
                             getClass().getResourceAsStream("/com/jorge_hugo_javier/Vistas/Pared.jpg"));
@@ -194,40 +161,12 @@ public class ControladorPrincipal {
                     }
                 } else {
                     panel.setStyle("-fx-background-color: red;");
-=======
-            Cell.Type tipo = celda.getType();
-
-            if (tipo == Cell.Type.WALL) {
-                Image imgPared = new Image(getClass().getResourceAsStream("/com/jorge_hugo_javier/Vistas/Pared.jpg"));
-                ImageView viewPared = new ImageView(imgPared);
-                viewPared.setFitWidth(40);
-                viewPared.setFitHeight(40);
-                panel.getChildren().add(viewPared);
-
-            } else if (tipo == Cell.Type.FLOOR) {
-                Image imgSuelo = new Image(getClass().getResourceAsStream("/com/jorge_hugo_javier/Vistas/suelo.png"));
-                ImageView viewSuelo = new ImageView(imgSuelo);
-                viewSuelo.setFitWidth(40);
-                viewSuelo.setFitHeight(40);
-                panel.getChildren().add(viewSuelo);
-
-                // Pintar jugador si está en esta celda
-                if (fila == jugador.getPosY() && col == jugador.getPosX()) {
-                    Image imgJugador = new Image(getClass().getResourceAsStream("/com/jorge_hugo_javier/Vistas/jugador.png"));
-                    ImageView viewJugador = new ImageView(imgJugador);
-                    viewJugador.setFitWidth(35);
-                    viewJugador.setFitHeight(35);
-                    panel.getChildren().add(viewJugador);
->>>>>>> Jorge
                 }
-            } else {
-                panel.setStyle("-fx-background-color: red;");
-            }
 
-            gridMapa.add(panel, col, fila);
+                gridMapa.add(panel, col, fila);
+            }
         }
     }
-<<<<<<< HEAD
 
     /**
      * Buscar hueco libre en el mapa para colocar al jugador
@@ -245,8 +184,5 @@ public class ControladorPrincipal {
             }
         }
     }
-=======
-}
->>>>>>> Jorge
 
 }
